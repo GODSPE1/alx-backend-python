e1884d5945d3f4ec0f59e86539ebb4c6b6ac2d82
GODSPE1 <godspy18@gmail.com>
2024-07-06 05:20:52 +0100

    add documentation

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-""""""
+"""This module defines an anotated multiplier function"""
 from typing import Callable
 
 
 def make_multiplier(multipier: float) -> Callable[[float], float]:
-    """"""
+    """returns the multiplier by a float"""
+
     def multiplier_function(val: float) -> float:
         return val * multipier
     return multiplier_function
