6aaa4aa5de0897e278d7bfa1b0387d3c8d2c5872
GODSPE1 <godspy18@gmail.com>
2024-07-29 18:03:05 +0100

    type annotated the function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 """Unit and Integration Tests for Utility Functions"""
-
 from parameterized import parameterized
-from typing import Mapping, Sequence
+from typing import Any, Mapping, Sequence
 from unittest import TestCase
+
+
+def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
+    current = nested_map
+    for key in path:
+        if key in current:
+            current = current[key]
+        else:
+            raise KeyError(f"Key {key} not found in map")
+        if not isinstance(current, dict) and key != path[-1]:
+            raise TypeError(f"Expected a dict at key {key},\
+                            got {type(current).__name__}")
+    return current
 
 
 class TestAccessNestedMap(TestCase):
@@ -17,6 +29,19 @@ class TestAccessNestedMap(TestCase):
     def test_access_nested_map(
             self, nested_dict: Mapping, path: Sequence, expected_value: Any
             ):
-        "Test that access_nested_map returns the correct value"""
+        """Test that access_nested_map returns the correct value"""
         self.assertEqual(access_nested_map(nested_dict, path), expected_value)
 
+    @parameterized.expand([
+        ({"a": 1}, ("a", "b")),
+        ({}, ("a",)),
+    ])
+    def test_access_nested_map_exception(
+            self, nested_dict: Mapping, path: Sequence):
+        """Test that access_nested_map raises KeyError for invalid paths"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_dict, path)
+
+
+if __name__ == '__main__':
+    unittest.main()
